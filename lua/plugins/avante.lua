7d097222a79e133fd2f61c4f7dcf6ebaf50cb001
xcritty <xuchen@infini-ai.com>
2025-10-07 17:14:23 +0800

    separated copilot config file

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -4,7 +4,6 @@ return {
 	build = "make",
 	event = "VeryLazy",
 	version = false,
-	-- enabled = false,
 	opts = {
 		-- add any opts here
 		-- this file can contain specific instructions for your project
@@ -15,12 +14,12 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
-		"nvim-mini/mini.pick", -- for file_selector provider mini.pick
+		"nvim-mini/mini.pick",     -- for file_selector provider mini.pick
 		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-		"ibhagwan/fzf-lua", -- for file_selector provider fzf
-		"stevearc/dressing.nvim", -- for input provider dressing
-		"folke/snacks.nvim", -- for input provider snacks
+		"hrsh7th/nvim-cmp",        -- autocompletion for avante commands and mentions
+		"ibhagwan/fzf-lua",        -- for file_selector provider fzf
+		"stevearc/dressing.nvim",  -- for input provider dressing
+		"folke/snacks.nvim",       -- for input provider snacks
 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 		"zbirenbaum/copilot.lua", -- for providers='copilot'
 		{
