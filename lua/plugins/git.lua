e5b8eca1563f0f09c7bdacbc9bcdaa3081611004
xcritty <xuchen@infini-ai.com>
2025-10-12 19:59:16 +0800

    refine git 2

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,16 +1,47 @@
 return {
-    "tpope/vim-fugitive",
-    {
-        "airblade/vim-gitgutter",
-        keys = {
-            { "<leader>hd", "<cmd>Gvdiffsplit<cr>", desc = "Git Diff File" },
-        },
-    },
+    { "tpope/vim-fugitive" },
     {
         "tanvirtin/vgit.nvim",
         event = "VimEnter",
         config = function()
-            require("vgit").setup()
+            require("vgit").setup({
+                keymaps = {
+                    ["n [c"] = "hunk_up",
+                    ["n ]c"] = "hunk_down",
+                    ["n <leader>gb"] = "buffer_blame_preview",
+                    ["n <leader>hp"] = "buffer_hunk_preview",
+                    ["n <leader>gp"] = "buffer_diff_preview",
+                    ["n <leader>gd"] = "project_diff_preview",
+                },
+                settings = {
+                    hls = {
+                        GitCount = 'Keyword',
+                        GitSymbol = 'CursorLineNr',
+                        GitTitle = 'Directory',
+                        GitSelected = 'QuickfixLine',
+                        GitBackground = 'Normal',
+                        GitAppBar = 'StatusLine',
+                        GitHeader = 'NormalFloat',
+                        GitFooter = 'NormalFloat',
+                        GitBorder = 'LineNr',
+                        GitLineNr = 'LineNr',
+                        GitComment = 'Comment',
+                        GitConflictCurrentMark = 'DiffAdd',
+                        GitConflictAncestorMark = 'Visual',
+                        GitConflictIncomingMark = 'DiffChange',
+                        GitConflictCurrent = 'DiffAdd',
+                        GitConflictAncestor = 'Visual',
+                        GitConflictMiddle = 'Visual',
+                        GitConflictIncoming = 'DiffChange',
+                    },
+                    scene = {
+                        diff_preference = 'split',
+                        keymaps = {
+                            quit = 'q'
+                        }
+                    },
+                },
+            })
         end
     },
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,10 @@
 return {
-	{
-		"sainnhe/sonokai",
-		config = function()
-			vim.g.sonokai_style = "maia"
-			vim.cmd.colorscheme("sonokai")
-		end,
-	},
-	{ "folke/tokyonight.nvim" },
+    {
+        "sainnhe/sonokai",
+        config = function()
+            vim.g.sonokai_style = "maia"
+            vim.cmd.colorscheme("sonokai")
+        end,
+    },
+    { "folke/tokyonight.nvim" },
 }
