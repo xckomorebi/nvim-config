a5208a5e5aefbf2d7a31ed17b895658574bae2de
xcritty <xuchen@infini-ai.com>
2025-10-15 06:26:04 +0800

    feat: switch from neotree to nvim-tree

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -5,11 +5,19 @@ return {
     keys = {
         -- Will use Telescope if installed or a vim.ui.select picker otherwise
         { "<leader>wr", "<cmd>AutoSession search<CR>", desc = "Session search" },
-        { "<leader>ws", "<cmd>AutoSession save<CR>", desc = "Save session" },
+        { "<leader>ws", "<cmd>AutoSession save<CR>",   desc = "Save session" },
         { "<leader>wa", "<cmd>AutoSession toggle<CR>", desc = "Toggle autosave" },
     },
 
     opts = {
-        suppressed_dirs = {"~/"},
-    }
+        suppressed_dirs = { "~/" },
+
+        bypass_save_filetypes = {
+            "NvimTree",
+        },
+        session_lens = {
+            picker = "telescope",
+            load_on_setup = true,
+        },
+    },
 }
---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,102 @@
+local function on_attach(bufnr)
+    local api = require("nvim-tree.api")
+
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    api.config.mappings.default_on_attach(bufnr)
+
+    vim.keymap.set("n", "u", api.tree.change_root_to_parent, opts("Up"))
+    vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
+-- NOTE: File Explorer
+return {
+    "nvim-tree/nvim-tree.lua",
+    keys = {
+        { "<leader>b", "<cmd>NvimTreeToggle<cr>",   desc = "Toggle file explorer" },
+        { "<leader>r", "<cmd>NvimTreeFindFile<cr>", desc = "Reveal file" },
+    },
+
+    -- stupid hack to let nvim-tree execute highlight linking after colorscheme is loaded
+    dependencies = {
+        "sainnhe/sonokai"
+    },
+
+    lazy = false,
+
+    cmd = {
+        "NvimTreeOpen",
+        "NvimTreeToggle",
+        "NvimTreeFocus",
+        "NvimTreeFindFile",
+        "NvimTreeFindFileToggle",
+    },
+    opts = {
+        on_attach = on_attach,
+        disable_netrw = true,
+        diagnostics = {
+            enable = true,
+            icons = {
+                hint = "󰌵",
+                info = "",
+                warning = "",
+                error = "",
+            },
+            show_on_dirs = true,
+        },
+        sync_root_with_cwd = true,
+        update_focused_file = {
+            enable = true,
+            update_cwd = false,
+            ignore_list = {},
+        },
+        git = {
+            enable = true,
+            ignore = true,
+            show_on_dirs = true,
+            show_on_open_dirs = true,
+            timeout = 5000,
+        },
+        view = {
+            width = 35,
+        },
+        filters = {
+            git_ignored = false,
+        },
+        renderer = {
+            indent_markers = {
+                enable = true
+            },
+            icons = {
+                git_placement = "after",
+                diagnostics_placement = "right_align",
+                glyphs = {
+                    default = "󰈚",
+                    symlink = "",
+                    folder = {
+                        default = "",
+                        empty = "",
+                        empty_open = "",
+                        open = "",
+                        symlink = "",
+                        symlink_open = "",
+                        arrow_open = "",
+                        arrow_closed = "",
+                    },
+
+                    git = {
+                        unstaged = "",
+                        staged = "✓",
+                        unmerged = "",
+                        renamed = "➜",
+                        untracked = "U",
+                        deleted = "",
+                        ignored = "◌",
+                    },
+                },
+            },
+        },
+    },
+}
