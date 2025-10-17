8deed7e9b87564820ee8a69d351639a93fbe8b6f
xcritty <xuchen@infini-ai.com>
2025-10-17 13:10:30 +0800

    refine: noice add filters

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -14,19 +14,44 @@ return {
     },
 
     opts = {
+        views = {
+            cmdline_output = {
+                enter = true,
+                relative = "win"
+            },
+            messages = {
+                relative = "win"
+            },
+        },
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+            },
+        },
         presets = {
+            bottom_search = true,
             command_palette = true,
+            long_message_to_split = true,
+            inc_rename = false,
+            lsp_doc_border = false,
         },
         routes = {
-            -- {
-            --     filter = { event = "msg_show", },
-            --     view = "notify",
-            --     opts = {
-            --         level = "info",
-            --         skip = false,
-            --         replace = true,
-            --     }
-            -- },
+            {
+                view = "cmdline_output",
+                filter = {
+                    event = "msg_show",
+                    kind = { "shell_out", "shell_err" },
+                },
+            },
+            {
+                view = "messages",
+                filter = {
+                    event = "msg_show",
+                    kind = { "list_cmd" },
+                },
+            },
         }
-    }
+    },
 }
