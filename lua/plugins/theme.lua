c306781f5b03f5fc526f3bc28f7d81ab34c5d5e9
xcritty <xuchen@infini-ai.com>
2025-10-18 14:44:27 +0800

    feat: sonokai use transparenct background

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,6 +3,7 @@ return {
         "sainnhe/sonokai",
         config = function()
             vim.g.sonokai_style = "maia"
+            vim.g.sonokai_transparent_background = 1
             vim.cmd.colorscheme("sonokai")
         end,
     },
