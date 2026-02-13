9cbf1abf6c488d07ff4a9dada58eee1b9f9e2f5e
xcritty <xuchen@infini-ai.com>
2026-02-13 13:06:58 +0800

    feat(remap): add terminal window navigation keymaps
    Add Ctrl+w+h/j/k/l keymaps in terminal mode to navigate between windows
    without needing to manually exit terminal mode first.

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,6 +2,12 @@ local keyset = vim.keymap.set
 
 keyset("t", "<C-q>", "<C-\\><C-N>", { desc = "Go to Normal mode" })
 
+-- Use Ctrl + w + h/j/k/l to navigate between windows in Terminal mode
+keyset("t", "<C-w>h", "<C-\\><C-N><C-w>h", { desc = "Go to left window" })
+keyset("t", "<C-w>j", "<C-\\><C-N><C-w>j", { desc = "Go to lower window" })
+keyset("t", "<C-w>k", "<C-\\><C-N><C-w>k", { desc = "Go to upper window" })
+keyset("t", "<C-w>l", "<C-\\><C-N><C-w>l", { desc = "Go to right window" })
+
 keyset("n", "<leader>tu", "<cmd>ToggleSonoakiTransparency<cr>")
 
 keyset("n", "<C-->", "<C-o>", { desc = "Go to previous location" })
